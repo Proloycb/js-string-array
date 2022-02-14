4b5fbb59250f018b7873dfdff43658f6dd17c3ea
Proloycb <proloychacroborty@gmail.com>
2022-02-14 09:19:53 +0600

    first commit

---
+++ case-convert.js
@@ -0,0 +1,9 @@
+const anthem = 'Amar Sonar Bangla Ami Tomay Valobashi';
+const search = 'valo' ;
+
+
+const userInput = 'blaCkPinK' ;
+const savedUserName = 'blackpink' ;
+if(userInput.toLowerCase() == savedUserName.toLowerCase()) {
+    console.log('user exists') ;
+}
